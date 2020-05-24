f4f56babbd785f2537fe3ee8030c4b72a1581312
mariajsan <60493676+mariajsan@users.noreply.github.com>
2020-05-23 23:05:36 -0700

    bootstrap

--- veterinaria/veterinaria.web/App_Start/RouteConfig.cs
+++ veterinaria/veterinaria.web/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace veterinaria.web
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Principal", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
