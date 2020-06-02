12550760754024db96092f86c61560f4823c2bca
mariajsan <60493676+mariajsan@users.noreply.github.com>
2020-06-02 01:05:20 -0700

    18/19

---
+++ veterinaria/veterinaria.web/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace veterinaria.web.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<veterinaria.web.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(veterinaria.web.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
---
+++ veterinaria/veterinaria.web/Models/Owner.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace veterinaria.web.Models
+{
+    public class Owner
+    {
+        
+        public int Id { get; set; }
+        [Display(Name = "Nombre")]
+        [MaxLength(50)]
+        [Required]
+        public string FirstName { get; set; }
+        [Display(Name = "Apellido")]
+        [MaxLength(50)]
+        [Required]
+        public string LastName { get; set; }
+        [Display(Name = "Dirrección")]
+        [MaxLength(400)]
+        public string Address { get; set; }
+        [Display(Name = "Teléfono")]
+        [MaxLength(20)]
+        [Required]
+        public string Phone { get; set; }
+        [Display(Name = "Correo")]
+        [MaxLength(50)]
+        public string Email { get; set; }
+    }
+}
