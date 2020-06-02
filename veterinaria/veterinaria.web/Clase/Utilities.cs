d100cce44d8cc012a6b69e6276f078ae853a1351
mariajsan <60493676+mariajsan@users.noreply.github.com>
2020-06-02 02:45:13 -0700

    utilities

---
+++ veterinaria/veterinaria.web/Clase/Utilities.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using veterinaria.web.Models;
+
+namespace veterinaria.web.Clase
+{
+    public class Utilities
+    {
+        readonly static ApplicationDbContext db = new ApplicationDbContext();
+
+        public void Dispose() {
+            db.Dispose();
+        }
+    }
+}
